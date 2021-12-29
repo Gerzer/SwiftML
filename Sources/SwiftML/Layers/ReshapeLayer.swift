d0f387c3c635372f8876cc5cfa0e1d800133aec7
Gabriel Jacoby-Cooper <gerzersoftware@icloud.com>
2021-12-29 16:28:22 -0500

    Add inline documentation for `ReshapeLayer`

--- Sources/SwiftML/Layers/ReshapeLayer.swift
+++ Sources/SwiftML/Layers/ReshapeLayer.swift
@@ -7,6 +7,7 @@
 
 import MLCompute
 
+/// A reshape layer.
 public struct ReshapeLayer: Layer {
 	
 	public private(set) var internalLayer: MLCLayer!
@@ -19,13 +20,21 @@ public struct ReshapeLayer: Layer {
 	
 	private let didSetDefaultBatchSize: Bool
 	
+	/// Creates a reshape layer.
+	/// - Parameter newShape: The new shape of the output.
+	/// - Important: The number of elements that’s specified by the new shape must be the same as the number of elements in the input tensor during training. Furthermore, the new batch size must be the same as the batch size of the input shape during training.
 	public init(newShape: Tensor.Shape) {
 		self.newShape = newShape
 		self.didSetDefaultBatchSize = false
 	}
 	
-	public init(newPrimaryAxis primaryAxis: Int, newSecondaryAxis secondaryAxis: Int) {
-		self.newShape = Tensor.Shape(primaryAxis: primaryAxis, secondaryAxis: secondaryAxis)
+	/// Creates a reshape layer.
+	/// - Parameters:
+	///   - newPrimaryAxis: The new primary axis of the output shape.
+	///   - newSecondaryAxis: The new secondary axis of the output shape.
+	/// - Important: The number of elements that’s specified by the new shape must be the same as the number of elements in the input tensor during training.
+	public init(newPrimaryAxis: Int, newSecondaryAxis: Int) {
+		self.newShape = Tensor.Shape(primaryAxis: newPrimaryAxis, secondaryAxis: newSecondaryAxis)
 		self.didSetDefaultBatchSize = true
 	}
 	
